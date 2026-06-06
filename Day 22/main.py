ff0bd245155d5be6260ff89da4a1e81908a5bd46
Kayla S <KaylaStiver3@gmail.com>
2026-06-05 21:30:14 -0400

    Day 22: Both paddles created responding to key presses.

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,12 +1,24 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.title("Pong")
 screen.tracer(0)
-screen.exitonclick()
+screen.title("Pong")
 
-enemy_paddle = Paddle()
-enemy_paddle.create()
+
+user_paddle = Paddle(-350, 0)
+enemy_paddle = Paddle(350, 0)
+
+screen.listen()
+screen.onkey(enemy_paddle.up, "Up")
+screen.onkey(enemy_paddle.down,"Down")
+screen.onkey(user_paddle.up, "w")
+screen.onkey(user_paddle.down,"s")
+
+game_continue = True
+while game_continue:
+    screen.update()
+
+screen.exitonclick()
--- Day 22/paddle.py
+++ Day 22/paddle.py
@@ -1,24 +1,21 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, x_cor, y_cor):
         super().__init__()
-        self.paddle_list = []
-
-    def create(self):
-        for square in range(4):
-            new_square = Turtle("square")
-            new_square.color("white")
-            new_square.penup()
-            self.paddle_list.append(new_square)
-        spacing = 0
-        for piece in self.paddle_list:
-            piece.goto(spacing, 0)
-            spacing += -20
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.speed("fastest")
+        self.goto(x_cor, y_cor)
 
     def up(self):
-        self.paddle_list[0].setheading(90)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def down(self):
-        self.paddle_list[0].setheading(270)
+        new_y =(self.ycor() - 20)
+        self.goto(self.xcor(), new_y)
+
 
