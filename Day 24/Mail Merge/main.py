62ccfc727f6a8dd77014cec7adb74e18e10237f5
Kayla S <KaylaStiver3@gmail.com>
2026-07-10 22:59:09 -0400

    Day 24: Mail merge start.

--- Day 24/Mail Merge/main.py
+++ Day 24/Mail Merge/main.py
@@ -6,3 +6,15 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt", mode="r") as file:
+    letter_contents = file.read()
+
+with open("./Input/Names/invited_names.txt", "r") as file:
+    name_list = file.read().splitlines()
+    print(name_list)
+
+for name in name_list:
+    letter_with_name = letter_contents.replace("[name]", name)
+    with open(f"./Output/ReadyToSend/letter_for_{name}", "w") as file:
+        file.write(letter_with_name)
