b2c496deb75d62ca855377a281e1403f9a32ac6b
Kayla S <KaylaStiver3@gmail.com>
2026-08-11 16:23:54 -0400

    Day 35: Complete.

--- Day 35/main.py
+++ Day 35/main.py
@@ -1,1 +1,30 @@
-api_key = "0a73c2eb5bc80f5f61d60e9143a0e765"
+import requests
+from vonage import Auth, Vonage
+from vonage_sms import SmsMessage, SmsResponse
+import os
+
+lat = 39.758949
+long = -84.191605
+owm_api_key = os.environ.get("owm_api_key")
+von_api_key = os.environ.get("von_api_key")
+api_secret = os.environ.get("api_secret")
+
+data = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={long}&cnt=4&appid={owm_api_key}")
+data.raise_for_status()
+intervals = data.json()["list"]
+
+raining = False
+for i in intervals:
+    for condition in i["weather"]:
+        if int(condition["id"]) < 700:
+            raining = True
+if raining:
+    client = Vonage(Auth(api_key=von_api_key, api_secret=api_secret))
+    message = SmsMessage(
+        to="19377015358",
+        from_="16265491364",
+        text="Bring an Umbrella!",
+    )
+
+    response: SmsResponse = client.sms.send(message)
+    print(response)
