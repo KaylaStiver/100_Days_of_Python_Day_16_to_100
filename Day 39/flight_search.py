74348615ac6ea9c0c9ff6dd19f7ee59ac4ab7a41
Kayla S <KaylaStiver3@gmail.com>
2026-08-17 20:18:14 -0400

    Day 39: Searching for flight info.

--- Day 39/flight_search.py
+++ Day 39/flight_search.py
@@ -1,3 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.app_id = os.getenv("X_APP_ID")
+        self.app_key = os.getenv("X_APP_KEY")
+        self.url = "https://app.100daysofpython.dev/v1/flights/search"
+
+    def check_flights(self, from_airport_code, to_airport_code, from_time, to_time):
+        parameters = {
+            "engine": "google_flights",
+            "departure_id": from_airport_code,
+            "arrival_id": to_airport_code,
+            "outbound_date": from_time,
+            "return_date": to_time,
+            "type": "1",
+            "adults": "1",
+            "currency": "GBP",
+            "api_key": self.app_key,
+        }
+
+        response = requests.get(self.url, params=parameters)
+        return response.json()
--- Day 39/main.py
+++ Day 39/main.py
@@ -1,9 +1,17 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
-import requests
 from data_manager import DataManager
+from flight_search import FlightSearch
 from pprint import pprint
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
 sheet_data = data_manager.get_data()
-pprint(sheet_data)
+print(sheet_data)
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_now = tomorrow + timedelta(days=182)
+
+flight_search = FlightSearch()
+# flight_search.check_flights(sheet_data[[]])
+
