539df39da4049cc6e2ec119a93d610f1a12e79d7
Kayla S <KaylaStiver3@gmail.com>
2026-08-21 14:37:02 -0400

    Day 39: Complete + implemented text message functionality.

--- Day 39/main.py
+++ Day 39/main.py
@@ -2,20 +2,23 @@
 
 from data_manager import DataManager
 from flight_search import FlightSearch
-from pprint import pprint
 from datetime import datetime, timedelta
 from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager, send_message
 
+#Obtain data from Google Sheet
 data_manager = DataManager()
 sheet_data = data_manager.get_data()
 
+#Format tomorrow's date and date six months from now
 tomorrow = datetime.now() + timedelta(days=1)
 six_months_from_now = tomorrow + timedelta(days=182)
 tomorrow = tomorrow.strftime("%Y-%m-%d")
 six_months_from_now = six_months_from_now.strftime("%Y-%m-%d")
 
-
+#Search for flights during this time period
 flight_search = FlightSearch()
+notif_flights = []
 for i in range(0,3):
     flight_data = flight_search.check_flights(
                                 from_airport_code="LHR",
@@ -23,7 +26,13 @@ for i in range(0,3):
                                 from_time=tomorrow,
                                 to_time=six_months_from_now,
     )
+#For each city in the Google Sheet, find the cheapest flight
     flight = find_cheapest_flight(flight_data, six_months_from_now)
-    pprint(f"{flight.destination_airport}: GBP {flight.price}")
+#If the cheapest flight available is lower than the Google Sheet price, save to a list
+    if flight.price < sheet_data[i]["price"]:
+        notif_flights.append(flight)
 
+#Send text message to notify of the cheapest available flights for each city
+notif_manager = NotificationManager()
+send_message(notif_manager, notif_flights)
 
--- Day 39/notification_manager.py
+++ Day 39/notification_manager.py
@@ -1,3 +1,22 @@
+from vonage import Auth, Vonage
+from vonage_sms import SmsMessage, SmsResponse
+import os
+from dotenv import load_dotenv
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.von_api_key = os.getenv("VON_API_KEY")
+        self.api_secret = os.getenv("API_SECRET")
+
+def send_message(self, notif_flights):
+    for flight in notif_flights:
+        client = Vonage(Auth(api_key=self.von_api_key, api_secret=self.api_secret))
+        message = SmsMessage(
+            to="19377015358",
+            from_="16265491364",
+            text=f"Low price alert! Only £{flight.price} to fly from {flight.origin_airport} to {flight.destination_airport}, "
+                 f"on {flight.out_date} to {flight.return_date}.",
+        )
+        response: SmsResponse = client.sms.send(message)
+        print(response)
